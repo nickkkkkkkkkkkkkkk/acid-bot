a24b22cad647ac37aa8aefe96dac242d7d523bb9
nick c <nick@dying.fr>
2022-04-19 02:56:25 -0400

    3

--- index.js
+++ index.js
@@ -3,20 +3,31 @@ require("dotenv").config
 
 const client = new Discord.Client({
     intents: [
-        "GUILDS",
+       "GUILDS",
         "GUILD_MESSAGES",
         "GUILD_MEMBERS"
     ]
 })
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+let bot = {
+    client,
+    prefix: ",",
+    owners: [684560921020203053]
+}
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hai :3")
-    }
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+
+module.exports = bot
+
+//client.on("ready", () => {
+//    console.log(`Logged in as ${client.user.tag}`)
+//})
+//
+//client.on("messageCreate", (message) => {
+//    if (message.content == "hi"){
+//        message.reply("Hai :3")
+//    }
+//})
 
 client.login(process.env.TOKEN)
